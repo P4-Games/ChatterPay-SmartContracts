cf0d22ded8f3ebb0dc58b8b8b2231dcfe9c436ef
dappsar <dapps.ar@gmail.com>
2025-04-20 15:09:16 -0300

    [refactor] :recycle: refactor chatterpayWalletProxy.sol code structure for better readability (#96)

--- src/ChatterPayWalletProxy.sol
+++ src/ChatterPayWalletProxy.sol
@@ -1,15 +1,23 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.24;
+
+/*//////////////////////////////////////////////////////////////
+// IMPORTS
+//////////////////////////////////////////////////////////////*/
 
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 /**
  * @title ChatterPayWalletProxy
+ * @author ChatterPay Team
  * @notice An upgradeable proxy contract using the ERC1967 standard.
  * @dev Delegates calls to an implementation address and supports upgrades.
  */
 contract ChatterPayWalletProxy is ERC1967Proxy {
+    /*//////////////////////////////////////////////////////////////
+    // INITIALIZATION
+    //////////////////////////////////////////////////////////////*/
+
     /**
      * @notice Initializes the proxy with an implementation and initialization data.
      * @dev Calls the ERC1967Proxy constructor with the implementation address and data.
@@ -18,6 +26,10 @@ contract ChatterPayWalletProxy is ERC1967Proxy {
      */
     constructor(address _logic, bytes memory _data) ERC1967Proxy(_logic, _data) {}
 
+    /*//////////////////////////////////////////////////////////////
+    // GETTER FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
     /**
      * @notice Retrieves the address of the current implementation.
      * @return The address of the implementation contract.
@@ -25,6 +37,10 @@ contract ChatterPayWalletProxy is ERC1967Proxy {
     function getImplementation() public view returns (address) {
         return _implementation();
     }
+
+    /*//////////////////////////////////////////////////////////////
+    // MAIN FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
 
     // Explicit receive function
     receive() external payable {}
