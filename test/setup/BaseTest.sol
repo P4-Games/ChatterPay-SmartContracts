7c89499877ac00890dee4f2acec8c0898e4397a2
dappsar <dapps.ar@gmail.com>
2025-04-20 12:33:48 -0300

    [refactor] :fire: remove unused DummyAggregator.sol

--- test/setup/BaseTest.sol
+++ test/setup/BaseTest.sol
@@ -7,7 +7,6 @@ import {ChatterPayWalletFactory} from "../../src/ChatterPayWalletFactory.sol";
 import {ChatterPayPaymaster} from "../../src/ChatterPayPaymaster.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {DummyAggregator} from "./DummyAggregator.sol";
 import {BaseConstants} from "./BaseConstants.sol";
 
 /*//////////////////////////////////////////////////////////////
