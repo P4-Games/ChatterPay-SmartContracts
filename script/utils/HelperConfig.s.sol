7f1c050ead43de4fd83bdedf5b4d55a56e27188b
dappsar <dapps.ar@gmail.com>
2026-01-06 12:07:06 -0300

    [feat] :sparkles: add usx testnet addresses (#190)

--- script/utils/HelperConfig.s.sol
+++ script/utils/HelperConfig.s.sol
@@ -76,8 +76,7 @@ contract HelperConfig is Script {
     uint256 constant LOCAL_CHAIN_ID = 31337;
 
     address constant BURNER_WALLET = 0x08f88ef7ecD64a2eA1f3887d725F78DDF1bacDF1;
-    address constant ANVIL_DEFAULT_ACCOUNT =
-        0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    address constant ANVIL_DEFAULT_ACCOUNT = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
     address immutable BACKEND_SIGNER;
 
     NetworkConfig public localNetworkConfig;
@@ -111,19 +110,13 @@ contract HelperConfig is Script {
      * @param chainId The blockchain network ID
      * @return NetworkConfig Configuration for the specified chain ID
      */
-    function getConfigByChainId(
-        uint256 chainId
-    ) public returns (NetworkConfig memory) {
+    function getConfigByChainId(uint256 chainId) public returns (NetworkConfig memory) {
         if (chainId == LOCAL_CHAIN_ID) {
             return getOrCreateAnvilEthConfig();
         } else if (networkConfigs[chainId].backendSigner != address(0)) {
             return networkConfigs[chainId];
         } else {
-            console.log(
-                "Invalid account %s for chainId: %s",
-                networkConfigs[chainId].backendSigner,
-                chainId
-            );
+            console.log("Invalid account %s for chainId: %s", networkConfigs[chainId].backendSigner, chainId);
             revert HelperConfig__InvalidChainId();
         }
     }
@@ -134,15 +127,10 @@ contract HelperConfig is Script {
      * @param symbol The symbol of the token to search for (e.g., "USDC", "USDT", "WETH")
      * @return token The ERC20 token address matching the given symbol
      */
-    function getTokenBySymbol(
-        string memory symbol
-    ) external view returns (address token) {
+    function getTokenBySymbol(string memory symbol) external view returns (address token) {
         NetworkConfig memory config = networkConfigs[block.chainid];
         for (uint256 i = 0; i < config.tokensConfig.length; i++) {
-            if (
-                keccak256(bytes(config.tokensConfig[i].symbol)) ==
-                keccak256(bytes(symbol))
-            ) {
+            if (keccak256(bytes(config.tokensConfig[i].symbol)) == keccak256(bytes(symbol))) {
                 return config.tokensConfig[i].token;
             }
         }
@@ -157,11 +145,7 @@ contract HelperConfig is Script {
      * @notice Gets configuration for Arbitrum Sepolia testnet
      * @return NetworkConfig Configuration with Arbitrum Sepolia addresses
      */
-    function getArbitrumSepoliaConfig()
-        public
-        view
-        returns (NetworkConfig memory)
-    {
+    function getArbitrumSepoliaConfig() public view returns (NetworkConfig memory) {
         TokenConfig[] memory tokenConfigs = new TokenConfig[](3);
 
         tokenConfigs[0] = TokenConfig({
@@ -185,29 +169,24 @@ contract HelperConfig is Script {
             isStable: true
         });
 
-        return
-            NetworkConfig({
-                entryPoint: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789,
-                backendSigner: BACKEND_SIGNER,
-                nftBaseUri: "https://dev.back.chatterpay.net/nft/metadata/opensea/",
-                tokensConfig: tokenConfigs,
-                uniswapConfig: UniswapConfig({
-                    router: 0x101F443B4d1b059569D643917553c771E1b9663E,
-                    factory: 0x248AB79Bbb9bC29bB72f7Cd42F17e054Fc40188e,
-                    positionManager: 0x6b2937Bde17889EDCf8fbD8dE31C3C2a70Bc4d65
-                })
-            });
+        return NetworkConfig({
+            entryPoint: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789,
+            backendSigner: BACKEND_SIGNER,
+            nftBaseUri: "https://dev.back.chatterpay.net/nft/metadata/opensea/",
+            tokensConfig: tokenConfigs,
+            uniswapConfig: UniswapConfig({
+                router: 0x101F443B4d1b059569D643917553c771E1b9663E,
+                factory: 0x248AB79Bbb9bC29bB72f7Cd42F17e054Fc40188e,
+                positionManager: 0x6b2937Bde17889EDCf8fbD8dE31C3C2a70Bc4d65
+            })
+        });
     }
 
     /**
      * @notice Gets configuration for Scroll Sepolia testnet
      * @return NetworkConfig Configuration with Scroll Sepolia addresses
      */
-    function getScrollSepoliaConfig()
-        public
-        view
-        returns (NetworkConfig memory)
-    {
+    function getScrollSepoliaConfig() public view returns (NetworkConfig memory) {
         TokenConfig[] memory tokenConfigs = new TokenConfig[](4);
 
         tokenConfigs[0] = TokenConfig({
@@ -238,18 +217,52 @@ contract HelperConfig is Script {
             isStable: false
         });
 
-        return
-            NetworkConfig({
-                entryPoint: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789,
-                backendSigner: BACKEND_SIGNER,
-                nftBaseUri: "https://dev.back.chatterpay.net/nft/metadata/opensea/",
-                tokensConfig: tokenConfigs,
-                uniswapConfig: UniswapConfig({
-                    router: 0x17AFD0263D6909Ba1F9a8EAC697f76532365Fb95,
-                    factory: 0xB856587fe1cbA8600F75F1b1176E44250B11C788,
-                    positionManager: 0xbbAd0e891922A8A4a7e9c39d4cc0559117016fec
-                })
-            });
+        tokenConfigs[4] = TokenConfig({
+            symbol: "SCR",
+            token: 0x0000000000000000000000000000000000000000,
+            priceFeed: 0x0000000000000000000000000000000000000000,
+            isStable: false
+        });
+
+        tokenConfigs[5] = TokenConfig({
+            symbol: "wstETH",
+            token: 0x0000000000000000000000000000000000000000,
+            priceFeed: 0x0000000000000000000000000000000000000000,
+            isStable: false
+        });
+
+        tokenConfigs[6] = TokenConfig({
+            symbol: "USX",
+            token: 0x7D2e101f2Ae45C9F97F9DaCaA7cBebF7dd92b96f,
+            priceFeed: 0x0000000000000000000000000000000000000000,
+            isStable: true
+        });
+
+        tokenConfigs[7] = TokenConfig({
+            symbol: "sUSX",
+            token: 0xccb541aa288cf3d82d60261895e333E78f1F70Ca,
+            priceFeed: 0x0000000000000000000000000000000000000000,
+            isStable: true
+        });
+
+        tokenConfigs[8] = TokenConfig({
+            symbol: "USDQ",
+            token: 0xaBB12F0315Bc523475eCA5CBd54aEEac3F9B983D,
+            priceFeed: 0x0000000000000000000000000000000000000000,
+            isStable: true
+        });
+
+        return NetworkConfig({
+            entryPoint: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789,
+            backendSigner: BACKEND_SIGNER,
+            nftBaseUri: "https://dev.back.chatterpay.net/nft/metadata/opensea/",
+            tokensConfig: tokenConfigs,
+            uniswapConfig: UniswapConfig({
+                router: 0x17AFD0263D6909Ba1F9a8EAC697f76532365Fb95,
+                factory: 0xB856587fe1cbA8600F75F1b1176E44250B11C788,
+                positionManager: 0xbbAd0e891922A8A4a7e9c39d4cc0559117016fec
+            })
+        });
     }
 
     /**
@@ -280,18 +293,17 @@ contract HelperConfig is Script {
             isStable: true
         });
 
-        return
-            NetworkConfig({
-                entryPoint: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789,
-                backendSigner: BACKEND_SIGNER,
-                nftBaseUri: "https://back.chatterpay.net/nft/metadata/opensea/",
-                tokensConfig: tokenConfigs,
-                uniswapConfig: UniswapConfig({
-                    router: 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45,
-                    factory: 0x1F98431c8aD98523631AE4a59f267346ea31F984,
-                    positionManager: 0xC36442b4a4522E871399CD717aBDD847Ab11FE88
-                })
-            });
+        return NetworkConfig({
+            entryPoint: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789,
+            backendSigner: BACKEND_SIGNER,
+            nftBaseUri: "https://back.chatterpay.net/nft/metadata/opensea/",
+            tokensConfig: tokenConfigs,
+            uniswapConfig: UniswapConfig({
+                router: 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45,
+                factory: 0x1F98431c8aD98523631AE4a59f267346ea31F984,
+                positionManager: 0xC36442b4a4522E871399CD717aBDD847Ab11FE88
+            })
+        });
     }
 
     /**
@@ -364,18 +376,17 @@ contract HelperConfig is Script {
             isStable: true
         });
 
-        return
-            NetworkConfig({
-                entryPoint: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789,
-                backendSigner: BACKEND_SIGNER,
-                nftBaseUri: "https://back.chatterpay.net/nft/metadata/opensea/",
-                tokensConfig: tokenConfigs,
-                uniswapConfig: UniswapConfig({
-                    router: 0xfc30937f5cDe93Df8d48aCAF7e6f5D8D8A31F636,
-                    factory: 0x70C62C8b8e801124A4Aa81ce07b637A3e83cb919,
-                    positionManager: 0xB39002E4033b162fAc607fc3471E205FA2aE5967
-                })
-            });
+        return NetworkConfig({
+            entryPoint: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789,
+            backendSigner: BACKEND_SIGNER,
+            nftBaseUri: "https://back.chatterpay.net/nft/metadata/opensea/",
+            tokensConfig: tokenConfigs,
+            uniswapConfig: UniswapConfig({
+                router: 0xfc30937f5cDe93Df8d48aCAF7e6f5D8D8A31F636,
+                factory: 0x70C62C8b8e801124A4Aa81ce07b637A3e83cb919,
+                positionManager: 0xB39002E4033b162fAc607fc3471E205FA2aE5967
+            })
+        });
     }
 
     /**
@@ -404,35 +415,17 @@ contract HelperConfig is Script {
 
         // 0: USDT, 1: WETH, 2: USDC
         TokenConfig[] memory tokenConfigs = new TokenConfig[](3);
-        tokenConfigs[0] = TokenConfig({
-            symbol: "USDT",
-            token: address(usdtMock),
-            priceFeed: address(0),
-            isStable: true
-        });
-        tokenConfigs[1] = TokenConfig({
-            symbol: "WETH",
-            token: address(wethMock),
-            priceFeed: address(0),
-            isStable: false
-        });
-        tokenConfigs[2] = TokenConfig({
-            symbol: "USDC",
-            token: address(usdcMock),
-            priceFeed: address(0),
-            isStable: true
-        });
+        tokenConfigs[0] = TokenConfig({symbol: "USDT", token: address(usdtMock), priceFeed: address(0), isStable: true});
+        tokenConfigs[1] =
+            TokenConfig({symbol: "WETH", token: address(wethMock), priceFeed: address(0), isStable: false});
+        tokenConfigs[2] = TokenConfig({symbol: "USDC", token: address(usdcMock), priceFeed: address(0), isStable: true});
 
         localNetworkConfig = NetworkConfig({
             entryPoint: address(entryPoint),
             backendSigner: BACKEND_SIGNER,
             nftBaseUri: "https://dev.back.chatterpay.net/nft/metadata/opensea/",
             tokensConfig: tokenConfigs,
-            uniswapConfig: UniswapConfig({
-                router: address(0),
-                factory: address(0),
-                positionManager: address(0)
-            })
+            uniswapConfig: UniswapConfig({router: address(0), factory: address(0), positionManager: address(0)})
         });
         return localNetworkConfig;
     }
